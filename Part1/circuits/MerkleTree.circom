5c829402284f4611ce90a20da16ff7911bbe8bd4
Krzysztof Szubiczuk <krzysztof.szubiczuk@gmail.com>
2022-05-18 23:55:35 +0200

    Solved

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -1,12 +1,54 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+include "../node_modules/circomlib/circuits/switcher.circom"; // I LOOKED FOR IT SO LONG, IT'S SO COOL!
+
+// To this solution I had to introduce few varables
 
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
 
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    // Constant variables
+    var MAX_LEAVES_NUMBER = 2**n;
+    var TREE_ELEMENTS_NUMBER = (MAX_LEAVES_NUMBER * 2) - 1;
+    // List of poseidon hash functions
+    component poseidon[TREE_ELEMENTS_NUMBER];
+    // Helper variables
+    signal level;
+    signal lowIndex;
+    signal highIndex;
+
+    level = MAX_LEAVES_NUMBER;
+    lowIndex = MAX_LEAVES_NUMBER;
+    highIndex = lowIndex + level/2;
+
+    // Hashing leaves
+    for (var i = 0; i < MAX_LEAVES_NUMBER; i++) {
+        poseidon[i] = Poseidon(2);
+        if (i % 2 == 0) {
+            poseidon[i].inputs[0] <== leaves[i];
+            poseidon[i].inputs[1] <== leaves[i+1];
+        }
+    }
+
+    while (level > 0) {
+        for (var i = lowIndex; i < highIndex; i++) {
+            if (i % 2 == 0 && highIndex <= 2**n -1) {
+                poseidon[i] = Poseidon(2);
+                poseidon[i].inputs[0] <== poseidon[i - level].out;
+                poseidon[i].inputs[1] <== poseidon[i - level + 1].out;
+            }
+        }
+        level = level \ 2;
+        lowIndex = highIndex;
+        highIndex = highIndex + level/2;
+    }
+
+    root <== poseidon[TREE_ELEMENTS_NUMBER - 1].out;
+
+
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -16,4 +58,37 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+
+    // add a component to compute root with Poseidon Hash
+    component pos_hash[n];
+    // add a switcher component
+    component switcher[n];
+    // compute first hash from first two elements
+    // if current index is 0 (left) then it means its our current hash
+    // if it is one then it means it is a proof element
+    pos_hash[0] = Poseidon(2);
+    switcher[0] = Switcher();
+
+    switcher[0].sel <== path_index[0];
+    switcher[0].L <== leaf;
+    switcher[0].R <== path_elements[0];
+
+    pos_hash[0].inputs[0] <== switcher[0].outL;
+    pos_hash[0].inputs[1] <== switcher[0].outR;
+
+    // compute rest of the hashes
+    for (var i = 1; i < n; i++) {
+        pos_hash[i] = Poseidon(2);
+        switcher[i] = Switcher();
+
+        switcher[i].sel <== path_index[i];
+        switcher[i].L <== pos_hash[i-1].out;
+        switcher[i].R <== path_elements[i];
+
+        pos_hash[i].inputs[0] <== switcher[i].outL;
+        pos_hash[i].inputs[1] <== switcher[i].outR;
+    }
+    // get final root of a Merkle Tree
+    root <== pos_hash[n-1].out;
+
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -3,18 +3,80 @@ pragma solidity ^0.8.0;
 
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
+import "hardhat/console.sol";
 
 contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
+    uint256 constant public MAX_LEAVES_NUMBER = 8;
+    uint256 constant public TREE_ELEMENTS_NUMBER = (MAX_LEAVES_NUMBER * 2) - 1;
+    
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+
+        // The solution here is naive, but because all leaves are 0 I think it is proper solution.
+        // Since it is initialization of a tree, and no variables are used, this should not be a problem.
+
+        hashes = new uint256[](TREE_ELEMENTS_NUMBER); // 15 = 8 (leaves) + 4 (branches) + 2 (branches) + 1 (root)
+        // filling first 8 slots (0-7) with 0 - those are leaves
+        for (uint256 i = 0; i < 8; i++) {
+           hashes[i] = 0;
+        }
+        // filling next 4 slots (8-11) with poseidon(0,0)
+        for (uint256 i = 8; i < 12; i++) {
+            hashes[i] = PoseidonT3.poseidon([hashes[0], hashes[1]]);
+        }
+        // filling next 2 slots (12-13)
+        for (uint256 i = 12; i < 14; i++) {
+            hashes[i] = PoseidonT3.poseidon([hashes[8], hashes[9]]);
+        }
+        // filling last slot 14
+        hashes[14] = PoseidonT3.poseidon([hashes[12], hashes[13]]);
+
+        root = hashes[14];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        // Variables needed for algorithm of hashing a tree
+        uint256 level = MAX_LEAVES_NUMBER;
+        // uint256 substraction = level/2;
+        uint256 lowIndex = MAX_LEAVES_NUMBER;
+        uint256 highIndex = MAX_LEAVES_NUMBER + level/2;
+
+        // Check if index is less than maximum number of leaves
+        require(index < MAX_LEAVES_NUMBER, "Number of leaves is to high");
+
+        // Find index of first empty leaf
+        for (uint256 i = 0; i < MAX_LEAVES_NUMBER; i++) {
+            if (hashes[i] == 0) {
+                hashes[i] = hashedLeaf;
+                index = i;
+                break;
+            }
+        }
+        
+        // Hash all levels of the tree from leaves
+        while (level > 0) {
+            for (uint256 i = lowIndex; i < highIndex; i++) {
+                if (i % 2 == 0) {
+                    hashes[i] = PoseidonT3.poseidon([hashes[i - level], hashes[i - level + 1]]);
+                }
+            }
+
+            level = level/2;
+            lowIndex = highIndex;
+            highIndex += level/2;
+
+            if (highIndex > TREE_ELEMENTS_NUMBER) {
+                break;
+            }
+        }
+
+        root = hashes[TREE_ELEMENTS_NUMBER - 1];
+        return root;
     }
 
     function verify(
@@ -25,5 +87,7 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        bool proofRootEqCurrRoot = input[0] == root;
+        return verifyProof(a, b, c, input) && proofRootEqCurrRoot;
     }
 }
